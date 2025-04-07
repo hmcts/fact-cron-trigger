151c54c451d9fbb47943dec562b146af4fe41854
amstevenson <addstevenson@hotmail.com>
2025-04-07 16:57:47 +0100

    optimising imports

--- src/main/java/uk/gov/hmcts/reform/fact/utils/CsvUtil.java
+++ src/main/java/uk/gov/hmcts/reform/fact/utils/CsvUtil.java
@@ -7,7 +7,10 @@ import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import uk.gov.hmcts.reform.fact.exceptions.JsonConvertException;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CsvUtil {
 
